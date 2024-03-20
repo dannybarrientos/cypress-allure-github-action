c98b22ce1745cd8baeb6d4e6b06f87dcd12cde98
Danny Andres Barrientos Agudelo <dannybarrientosagudelo@gmail.com>
2024-03-19 21:59:05 -0500

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -29,7 +29,7 @@ module.exports = defineConfig({
     env: {
       allureReuseAfterSpec: true,
       allureSkipAutomaticScreenshots: true,
-      allureSkipAutomaticScreenshots: true,
+      allureAddVideoOnPass: true,
     },
   },
   reporter: 'junit',
